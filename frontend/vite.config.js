5fcc9b68804400d522e6e01c1403e99c1f94f030
qthais <thaiallb63@gmail.com>
2025-02-28 19:38:18 +0700

    delete vite proxy config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      "/api":{
-        target:import.meta.env.VITE_API_BASE_URL,
-        changeOrigin: true,
-        timeout:5000,
-      }
-    }
-  }
 })
