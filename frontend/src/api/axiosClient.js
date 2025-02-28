45282a627fd85a455f28e6853a51edb50eba4f62
qthais <thaiallb63@gmail.com>
2025-02-28 19:12:23 +0700

    fix code can run both in dev and production

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const axiosClient = axios.create({
-    baseURL: 'https://vietsubmovies.onrender.com',
+    baseURL: API_BASE_URL,
     withCredentials:true,
 })
 export default axiosClient
