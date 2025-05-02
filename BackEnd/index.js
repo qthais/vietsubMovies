b26a1a506f120afef5bf6fd0a76535157eaec44e
qthais <thaiallb63@gmail.com>
2025-05-02 10:09:36 +0700

    enhance(autoloadMovies)

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,32 +1,43 @@
-const express= require('express')
-const Routes= require('./routes/routes')
-const path= require('path')
-const {loadOphimMovies}=require('./config/init')
-const ENV_VARS = require('./config/vars')
-const db=require('./config/db')
-const cookieParser = require('cookie-parser')
-const app=express()
-const cors = require('cors')
-const port=ENV_VARS.PORT
-db.connect()
-app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:4173', 'https://cinestream.onrender.com'], // ✅ Allow only specific domains
+const express = require("express");
+const Routes = require("./routes/routes");
+const path = require("path");
+const { loadOphimMovies } = require("./config/init");
+const ENV_VARS = require("./config/vars");
+const db = require("./config/db");
+const cookieParser = require("cookie-parser");
+const app = express();
+const cors = require("cors");
+const port = ENV_VARS.PORT;
+db.connect();
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:4173",
+      "https://cinestream.onrender.com",
+    ], // ✅ Allow only specific domains
     credentials: true,
-    methods: ['GET','HEAD','PATCH', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific methods
-}));
+    methods: ["GET", "HEAD", "PATCH", "POST", "PUT", "DELETE", "OPTIONS"], // Allow specific methods
+  })
+);
 
-app.use('/api/avatarImages', express.static(path.join(__dirname, 'public/avatarImages')));
-app.use(cookieParser())
+app.use(
+  "/api/avatarImages",
+  express.static(path.join(__dirname, "public/avatarImages"))
+);
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-Routes(app)
-app.listen(port,()=>{
-    console.log(`Server started at port ${port}`)
-})
-app.get('/status',(req,res)=>{
-    res.status(200).json({
-        message:"Server is alive!"
-    })
-})
-loadOphimMovies(100)
+Routes(app);
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
+app.get("/status", (req, res) => {
+  res.status(200).json({
+    message: "Server is alive!",
+  });
+});
+setTimeout(() => {
+  loadOphimMovies(10);
+}, 1000 * 60 * 60 * 24 * 7);
 //czJMSeHQEl6MeBie
